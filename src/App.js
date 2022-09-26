df5e6ce8660ab87fb7b985c1c238fa15674aa1a4
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-26 12:04:43 -0700

    Add react-router skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
-  // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+
+      <Switch>
+        <Route path="/books/:id">
+          <BookDetail />
+        </Route>
+        <Route path="/books">
+          <BookList />
+        </Route>
+        <Route path="*">
+          <Redirect to="/books" />
+        </Route>
+      </Switch>
     </main>
   );
 }
